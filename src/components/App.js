899f50164cdd5bd887988406351da8e7dba717e9
Amit Raj <amitraj9046@gmail.com>
2020-11-09 16:56:15 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <p id="data-ns-test" value="project-name">
+        Agarbati Business
+      </p>
+      <p id="data-ns-test" value="project-description">
+        A small application.
+      </p>
+    </>
+  );
+};
 
 export default App;
